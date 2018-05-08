0d6bafb3270f9c32747c9577d3e1c2e78c55d1f9
costaleonardo <costaleonardo97@gmail.com>
2018-05-07 23:35:34 -0400

    Add algorithm #3.

---
+++ js/03-display-window-content.js
@@ -0,0 +1,12 @@
+/*
+
+  3. Print window content
+  - Write a JavaScript program to print the contents of the current window.
+
+*/
+
+const printWindow = () => {
+  window.print();
+}
+
+printWindow();
